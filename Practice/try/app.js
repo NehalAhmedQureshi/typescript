73f75011e819ff789c241075975d2202dc1082fc
Nehal <m.nehalq125@gmail.com>
2024-07-14 12:47:35 -0700

    edit readme and some practice

---
+++ Practice/try/app.js
@@ -0,0 +1,2 @@
+var user = { name: 'nehal', age: 17, gender: 'male', isMuslim: true, education: 'intermediate', isMarriage: false, jobLess: true, hobby: ['coding', 'learning', 'teaching', 'reciting'] };
+console.log("🚀 ~ user:", user.hobby[2]);
--- typescript.md
+++ typescript.md
@@ -1,3 +1,4 @@
 ### *important points*
 *  tsc 13-July.ts --outFile 13-July.js   ->   for covert typescript to javascript
 - tsc filename.ts -w   ->   for make terminal as live server
+- add class , assignment & practice folder .
